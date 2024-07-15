4b32ee57e16be8e7214855b3e4b01ebc4d55c6a1
Rpsingh <161583295+rpsingh171999@users.noreply.github.com>
2024-07-15 12:08:38 +0000

    DataTypes

---
+++ Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 45878745
+let accountEmail = "rpsingh@yopmail.com"
+var accountPswd = "12345";
+accountCity="Jaipur"
+let accountState;
+
+// accountId = 545
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPswd, accountCity, accountState]);
---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";  //Treat all JS code as newer version
+
+// alert(3 + 3); // We are using nodeJs not browser (It's use in Browser)
+
+// DataTypes
+
+// Number => 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value (It's a object type in JS)
+// undefined => value not defined
+// Symbol => use for unique value
+
+// object
+
+console.log(typeof null)
+console.log(typeof undefined)
