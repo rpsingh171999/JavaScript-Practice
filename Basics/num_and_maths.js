10ebbd2dcd4050d89d8b70960b6e5eed99cbdddb
Rpsingh <161583295+rpsingh171999@users.noreply.github.com>
2024-07-24 11:41:08 +0000

    Number and Math library

---
+++ Basics/num_and_maths.js
@@ -0,0 +1,14 @@
+const score = 100;
+console.log(score);
+
+const balance = new Number(100.437987987);
+console.log(balance);
+
+console.log(balance.toFixed(2)); // fix tha value after decimal
+
+console.log(balance.toPrecision(5)); // Start se lekar kitne decimal tak ki value leni h
+
+
+console.log(10**2);
+
+console.log(Math.random()); // It's always gives the value between 0 and 1
